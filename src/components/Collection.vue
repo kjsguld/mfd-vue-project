6c1b24da49f57f056fba50858455388dd50438fa
Kristian Jeppe Schou Guld <kjsguld@gmail.com>
2018-02-21 20:20:28 +0100

    collection newsletter

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -2,6 +2,12 @@
   <div>
     <img class="header-image-collection full-width-img" src="/static/img/Collection.jpg" alt="New collection">
     <product-list :filters="true" ></product-list>
+    <section class="collection__newsletter">
+      <form class="newsletter-form" action="">
+        <input class="newsletter-form__email btn--outline-blue" type="email" placeholder="your email">
+        <button class="newsletter-form__btn btn--grey" type="submit">Subscripe to our newsletter</button>
+      </form>
+    </section>
     <so-me></so-me>
   </div>
 </template>
@@ -20,4 +26,38 @@ export default {
 
 <style lang="scss" scoped>
 
+@import '../assets/styles/master';
+
+.collection__newsletter{
+  background-color: $lightblue;
+  margin: 5rem auto;
+  padding: 4rem 0;
+
+  form.newsletter-form{
+    max-width: 1170px;  
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 270px 270px 1fr;
+    grid-gap: 30px;
+    grid-template-areas: " . email btn . "; 
+    .newsletter-form__email{
+      grid-area: email;
+      text-transform: none;
+      cursor: default;
+      &::placeholder{
+        text-transform: uppercase;
+      }
+    }
+    .newsletter-form__btn{
+      grid-area: btn;
+      background-color: $white;
+      color: $text-color;
+    }
+    .newsletter-form__btn, .newsletter-form__email{
+      font-size: .85rem;
+      padding-top: .8rem;
+      padding-bottom: .8rem;
+    }
+  }
+}
 </style>
